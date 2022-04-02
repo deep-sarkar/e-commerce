284349b13e130f7b705043b5abd422cdae5fa940
deep-sarkar <deepsarkar96@gmail.com>
2022-04-02 23:36:18 +0530

    Added routes

--- thor/main.py
+++ thor/main.py
@@ -1,11 +1,32 @@
 from flask import Flask
+from flask_restful import Api
+
+from db.setup import connect_db
+from routes import all_routes
+
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/')
 def home():
     return 'Hello World'
+
+
+# db
+connect_db()
+
+
+# routes
+def activate_all_routes():
+    for route in all_routes:
+        end_point = route[0]
+        handler = route[1]
+        api.add_resource(handler, end_point)
+
+
+activate_all_routes()
 
 
 if __name__ == "__main__":
