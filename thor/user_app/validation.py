a734e5c7a26825eaae682b507ba43d5970e2ee17
deep-sarkar <deepsarkar96@gmail.com>
2022-04-02 23:39:36 +0530

    Added model field validators

---
+++ thor/user_app/validation.py
@@ -0,0 +1,23 @@
+import re
+from mongoengine.errors import ValidationError
+
+
+def validate_mobile_number(mobile):
+    regex = r'^[6-9]{1}\d{9}$'
+    match = re.match(regex, mobile)
+    if not match:
+        raise ValidationError('Invalid mobile number {}'.format(mobile))
+
+
+def validate_pin_number(pin):
+    regex = r'^[0-9]{6}$'
+    match = re.match(regex, pin)
+    if not match:
+        raise ValidationError('Invalid pin-code {}'.format(pin))
+
+
+def validate_password(password):
+    regex = r'^[A-Za-z0-9]{8,}$'
+    match = re.match(regex, password)
+    if not match:
+        raise ValidationError('password should be alpha numerical and min 8 digit len')
