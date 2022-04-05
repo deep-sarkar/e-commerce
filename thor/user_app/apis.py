8ffced2657f526d7773ed78bf9998659065743e8
deep-sarkar <deepsarkar96@gmail.com>
2022-04-05 21:25:12 +0530

    Added email service in registration API

--- thor/user_app/apis.py
+++ thor/user_app/apis.py
@@ -1,12 +1,15 @@
 import json
 
 from flask_restful import Resource
-from flask import request
+from flask import request, render_template
+from flask.views import View
 from mongoengine import Q
 
 from mongoengine.errors import ValidationError
 
 from .models import User, Address
+from .utils import send_account_activation_mail
+
 
 class UserRegisterAPI(Resource):
 
@@ -19,6 +22,7 @@ class UserRegisterAPI(Resource):
         gender = req_data.get('gender')
         password = req_data.get('password')
         conf_password = req_data.get('conf_password')
+
         if not all([first_name, last_name, mobile, email, password, conf_password]):
             return {'error': 'All fields are mandatory', 'code': 300}
 
@@ -26,6 +30,7 @@ class UserRegisterAPI(Resource):
             return {'error': 'password not matching', 'code': 301}
 
         user = User.objects(Q(email=email) | Q(mobile=mobile)).first()
+
         if user:
             return {'error': 'email/mobile no already in use', 'code': 302}
 
@@ -37,8 +42,11 @@ class UserRegisterAPI(Resource):
                         gender=gender,
                         password=password)
             user.save()
+
+            send_account_activation_mail(user)
         except ValidationError as e:
             return {'error': e.to_dict(), 'code': 300}
         except Exception as e:
             return {'error': str(e), 'code': 300}
         return {'message': 'success', 'code': 200}
+
